a31de725d60ddd7440c5e708bb22c4496d6567b4
Dann-Cooney <32134227+Dann-Cooney@users.noreply.github.com>
2017-10-10 17:41:32 +0100

    Add files via upload

---
+++ Lab 3/MutalExclusion.cpp
@@ -0,0 +1,32 @@
+#include "Semaphore.h"
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+int count = 0;
+
+void taskOne(std::shared_ptr<Semaphore> mutex)
+{
+ mutex ->Wait();
+ count = count + 1;
+ std::cout << count << std::endl;
+ mutex ->Signal();
+}
+
+
+int main(void)
+{
+  std::thread threadOne, threadTwo;
+  std::shared_ptr<Semaphore> mutex( new Semaphore);
+  
+  /**< Launch the threads  */
+  mutex ->Signal();
+
+  threadOne=std::thread(taskOne, mutex);
+  threadTwo=std::thread(taskOne, mutex);
+  
+  std::cout << "Launched from the main\n";
+  threadOne.join();
+  threadTwo.join();
+  return 0;
+}
