ad47cafedb3c7f2c593f331429b4f866cb9717d2
Dann-Cooney <32134227+Dann-Cooney@users.noreply.github.com>
2017-10-19 23:26:46 +0100

    Lab 4: Barriers

---
+++ Lab 4/Barriers.cpp
@@ -0,0 +1,65 @@
+
+#include "Semaphore.h"
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+
+int count = 0;
+int numberOfTimes = 2;
+
+void taskOne(std::shared_ptr<Semaphore> mutex,std::shared_ptr<Semaphore> BarrierA, std::shared_ptr<Semaphore> BarrierB)
+{
+ 
+  mutex->Wait();
+  std::cout <<"Mutex Wait" << std::endl;
+
+  count++;
+  std::cout <<"Thread:"<< count << std::endl;
+ 
+ 
+  if(count == numberOfTimes)
+   {
+      std::cout <<"Both Threads are at the barrier" << std::endl;
+      BarrierB ->Wait();
+      BarrierA ->Signal();
+   }
+ 
+  mutex->Signal();
+  std::cout <<"Mutex Signal " << std::endl;
+
+  BarrierA -> Wait();
+  BarrierA -> Signal();
+ 
+  mutex ->Wait();
+
+  count--;
+
+  if(count == 0)
+    {
+      std::cout <<"Threads are finished" << std::endl;      
+      BarrierA -> Wait();
+      BarrierB ->Signal();
+    }
+
+   mutex->Signal();
+   BarrierB -> Wait();
+   BarrierB -> Signal();
+
+}
+int main(void)
+{
+  std::thread threadOne, threadTwo;
+  std::shared_ptr<Semaphore> mutex( new Semaphore(1));
+  std::shared_ptr<Semaphore> BarrierA( new Semaphore);
+  std::shared_ptr<Semaphore> BarrierB( new Semaphore(1));
+  
+
+  threadOne = std::thread(taskOne,mutex,BarrierA,BarrierB);
+  threadTwo = std::thread(taskOne,mutex,BarrierA,BarrierB);
+
+  std::cout << "Launched from the main\n";
+  threadOne.join();
+  threadTwo.join();
+  return 0;
+}
